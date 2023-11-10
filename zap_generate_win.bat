d87d5ce33dc575cc232033f5abcfb09a250dc3c3
Silvio Vallorani <silvio.vallorani.dev@gmail.com>
2023-11-10 15:03:46 +0100

    feat(TASKS): add zap tasks for windows

---
+++ zap_generate_win.bat
@@ -0,0 +1,2 @@
+echo "Generate CPP files from ZAP Project"
+python.exe c:\\ncs\\v2.4.2\\modules\\lib\\matter\\scripts\\tools\\zap\\generate.py c:\\MATTER\\device_wifi\\src\\template.zap -t c:\\ncs\\v2.4.2\\modules\\lib\\matter\\src\\app\\zap-templates\\app-templates.json -o c:\\MATTER\\device_wifi\\src\\zap-generated\\
---
+++ zap_modify_win.bat
@@ -0,0 +1,2 @@
+echo "Start ZAP Toooland edit ZAP file"
+zap c:\\MATTER\\device_wifi\\src\\template.zap --zcl c:\\ncs\\v2.4.2\\modules\\lib\\matter\\src\\app\\zap-templates\\zcl\\zcl.json --gen c:\\ncs\\v2.4.2\\modules\\lib\\matter\\src\\app\\zap-templates\\app-templates.json
